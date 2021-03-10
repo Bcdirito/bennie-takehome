049cd70bc35f4d09ce1d0f8b6fb4f82937cbe17a
Brian DiRito <bdirito91@gmail.com>
2021-03-10 12:00:01 -0500

    form tablet styling

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,34 +1,36 @@
 import '../styling/NewUserForm.scss'
+import backArrow from "../assets/backArrow.svg"
 
 const NewUserForm = (props) => {
     return (
-        <div id="formContainer" onClick={(e) => props.clickHandler(e)}>
+        <div id="formContainer" onClick={(e) => props.hideForm(e)}>
             <div id="newUserForm">
+                <img src={backArrow} alt="Back To Homepage" id="xIcon" onClick={(e) => props.hideForm(e)} />
                 <h1>Create New User:</h1>
                 <form onSubmit={(e) => props.submitHandler(e)}>
                     <div className="inputContainer">
                         <label>Full Name:</label>
-                        <input type="text" name="name" onChange={(e) => props.changeHandler(e)}/>            
+                        <input type="text" name="name" required="required" onChange={(e) => props.changeHandler(e)}/>            
                     </div>
                     <div className="inputContainer">
                         <label>Username:</label>
-                        <input type="text" name="username" onChange={(e) => props.changeHandler(e)}/>
+                        <input type="text" name="username" required="required" onChange={(e) => props.changeHandler(e)}/>
                     </div>
                     <div className="inputContainer">
                         <label>Email:</label>
-                        <input type="email" name="email" onChange={(e) => props.changeHandler(e)}/>
+                        <input type="email" name="email" required="required"  onChange={(e) => props.changeHandler(e)}/>
                     </div>
                     <div className="inputContainer">
                         <label>Address:</label>
-                        <input type="text" name="address" onChange={(e) => props.changeHandler(e)}/>
+                        <input type="text" name="address" required="required" onChange={(e) => props.changeHandler(e)}/>
                     </div>
                     <div className="inputContainer">
                         <label>Website:</label>
-                        <input type="text" name="address" onChange={(e) => props.changeHandler(e)}/>
+                        <input type="text" name="address" required="required"  onChange={(e) => props.changeHandler(e)}/>
                     </div>
                     <div className="inputContainer">
                         <label>Phone:</label>
-                        <input type="text" name="address" onChange={(e) => props.changeHandler(e)}/>
+                        <input type="text" name="address" required="required"  onChange={(e) => props.changeHandler(e)}/>
                     </div>
                     <input type="submit" value="Submit User" />
                 </form>
