347a77f6af57695c5761626e8240f32a368ad248
Brian DiRito <bdirito91@gmail.com>
2021-03-10 20:29:11 -0500

    capitalize new user name

--- src/components/home/UserCard.js
+++ src/components/home/UserCard.js
@@ -25,7 +25,7 @@ const UserCard = (props) => {
                         posts: posts
                     }
                 }}
-            >{user.username}</Link>
+            >{user.name}</Link>
             <ul>{renderPostTitles(user.username, posts)}</ul>
         </div>
     )
